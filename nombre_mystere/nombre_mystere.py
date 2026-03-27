8c2fbcca1571839db8b21fcd34957ef592250010
alakou <alakoustone@gmail.com>
2026-03-27 14:09:26 +0100

    nombre mistere handle correct

--- nombre_mystere/nombre_mystere.py
+++ nombre_mystere/nombre_mystere.py
@@ -1,95 +1,92 @@
-# < pour >
+########## LEVEL ############################################################""
 import random
 
 
-def label(l: str) -> str:
+def label(l: int):
+    if l == 1:
+        print(f"LEVEL {l} (RANDOM BETWEEN 1 - 50)")
 
-    if l == "1":
-        return f"LEVEL {l} (GUESS BETWEEN 1 - 50)"
-    elif l == "2":
-        return f"LEVEL {l} (GUESS BETWEEN 1 - 100)"
-    elif l == "3":
-        return f"LEVEL {l} (GUESS BETWEEN 1 - 1000)"
-    else:
-        return f"{l} > INVALID LEVEL NUMBER | READ MENU"
+    elif l == 2:
+        print(f"LEVEL {l} (RANDOM BETWEEN 1 - 100)")
 
-
-def random_number(level: str) -> int:
-
-    if level == "1":
-        return random.randint(1, 50)
-    elif level == "2":
-        return random.randint(1, 100)
-    elif level == "3":
-        return random.randint(1, 1000)
-    else:
-        return 0
+    elif l == 3:
+        print(f"LEVEL {l} (RANDOM BETWEEN 1 - 1000)")
 
 
-def validate_input(msg):
+def valid_level_input() -> int:
     while True:
         try:
-            n = int(input(msg).strip())
-            if n <= 0:
-                raise ValueError
-            return n
+            l: int = int(input("Choose level: "))
+            if l not in (1, 2, 3):
+                print("ERROR 123 ")
+                continue
+            return l
         except ValueError:
-            print("Enter positive int numbr")
+            print("ValueError")
+
+
+def handel_level():
+    l = valid_level_input()
+    label(l)
+
+
+##########################################
+def handle_guess_number_input() -> int:
+    while True:
+        try:
+            g: int = int(input("Enter guess: "))
+            return g
+        except ValueError:
+            print("guess valueroor")
+
+
+# Lien entre valid_level_input() et random_guess()
+def random_guess(l: int) -> int:
+    if l == 1:
+        return random.randint(1, 50)
+    elif l == 2:
+        return random.randint(1, 100)
+    elif l == 3:
+        return random.randint(1, 1000)
 
 
 def main():
-
-    score: int = 0
-    life: int = 5
-    trackScor = 0
-    essai = 0
+    l: int = valid_level_input() # Choose level
+    label(l)
+    rn: int = random_guess(l) # random guess number by level
+    print(rn)
+    (life, attempts) = (5, 0)
+    print(f"outside {life}")
+    chance = life
     while True:
-        # Select Level      
-        levelinput: str = input("choose level>>> ")
+        g: int = handle_guess_number_input()
+        ##########################################
         
-        # Display level
-        r: str = label(levelinput)
-        if not r == "1":
-            continue
-    
-        print(r)
-        
-        # Generate random according to the level
-        r = random_number(levelinput)
-        print(f"RANDOM ==== {r}")
-        # Enter guess
-        while True :
-            guess = validate_input("Enter guess >>> ")
-            if life != 0:
-                if guess > r :
-                    life -= 1
-                    trackScor += 0
-                    essai += 1
-                    print(f"{guess} IS TOO LARGE")
-                    print(f"ATTEMPTS {essai}")
-                    continue
-                # Si guess sup à random number
-                elif guess <  r :
-                    life -= 1
-                    score += 0
-                    essai += 1
-                    print(f"{guess} IS TOO SMALL")
-                    print(f"ATTEMPTS {essai}")
-                    continue
-                else :
-                    life -= 1
-                    score += 1
-                    essai += 1
-                    print(f"{guess} CORRECT")
-                    print(f"SCORE >  {score} / {essai}")
-                    break
-            if life == 0 and guess != r :
-                print(f"GAME END")
-                print(f"SCORE >>>>>>>>>  {score} / {essai} attempts")
-                break
+        if life == 0 or g == rn:
+            print("GAME END !!")
             
-        break
+            print("Score = {0} / {1}".format(attempts, chance))
+            break
+        
+        if g > rn :
+            attempts += 1
+            life -= 1
+            print(f"{g} IS TOO LARGE")
+            print(f"{attempts} ATTEMPTS")
+            print(f"LLEFT {life} LIFE")
+            
+            continue
+        elif g < rn :
+            attempts += 1
+            life -= 1
+            print(f"{g} IS TOO SMALL")
+            print(f"{attempts} ATTEMPTS")
+            print(f"LLEFT {life} LIFE")
+            continue
             
 
+# >>>>>>>>> <<<<
 if __name__ == "__main__":
     main()
+
+
