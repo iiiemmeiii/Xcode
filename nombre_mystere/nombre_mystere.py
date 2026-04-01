21532764b7111e0a9c8263643c0a9a02b6199a1b
alakou <alakoustone@gmail.com>
2026-04-01 03:27:10 +0200

    Nombre mister fini

--- nombre_mystere/nombre_mystere.py
+++ nombre_mystere/nombre_mystere.py
@@ -16,28 +16,24 @@ def label(l: int):
 def valid_level_input() -> int:
     while True:
         try:
-            l: int = int(input("Choose level: "))
+            l: int = int(input("Choose level >>> "))
             if l not in (1, 2, 3):
-                print("ERROR 123 ")
+                print("ENTER LEVEL BETWEEN (1 - 2 - 3) ")
+
                 continue
             return l
         except ValueError:
-            print("ValueError")
-
-
-def handel_level():
-    l = valid_level_input()
-    label(l)
+            print("INVALID LEVEL: ONLY INTERGER NUMBER")
 
 
 ##########################################
 def handle_guess_number_input() -> int:
     while True:
         try:
-            g: int = int(input("Enter guess: "))
+            g: int = int(input("Enter guess >>> "))
             return g
         except ValueError:
-            print("guess valueroor")
+            print("ENTER INTEGER VALUE")
 
 
 # Lien entre valid_level_input() et random_guess()
@@ -50,43 +46,71 @@ def random_guess(l: int) -> int:
         return random.randint(1, 1000)
 
 
+def saveScoreFile(score: int):
+    (filename, action) = ("score.txt", "w")
+
+    try:
+        with open(filename, action) as file:
+            file.write(f"SCORE >>> {score} ATTEMPTS / 5")
+    except Exception as e:
+
+        (log, action) = ("log.txt", "w")
+        with open(log, action) as logF:
+            logF.write(f"Exception : \n{e}")
+
+
+def menu():
+    print(
+        """
+####### WELCOME TO GUESS GAME ########
+CHOOSE LEVEL : 
+    >>> 1 - EASY
+    >>> 2 - MEDIUM
+    >>> 3 - DIFFICULT
+
+ENTER THE GUESS NUMBER >>>
+YOU GET 5 LIVES
+          """
+    )
+
+
 def main():
-    l: int = valid_level_input() # Choose level
+    menu()
+    l: int = valid_level_input()  # Choose level
     label(l)
-    rn: int = random_guess(l) # random guess number by level
+    rn: int = random_guess(l)  # random guess number by level
     print(rn)
     (life, attempts) = (5, 0)
-    print(f"outside {life}")
     chance = life
     while True:
         g: int = handle_guess_number_input()
         ##########################################
-        
-        if life == 0 or g == rn:
+
+        if life == 1 or g == rn:
+            attempts += 1
             print("GAME END !!")
-            
+
             print("Score = {0} / {1}".format(attempts, chance))
+            saveScoreFile(attempts)
             break
-        
-        if g > rn :
+
+        if g > rn:
             attempts += 1
             life -= 1
             print(f"{g} IS TOO LARGE")
             print(f"{attempts} ATTEMPTS")
-            print(f"LLEFT {life} LIFE")
-            
+            print(f"LEFT {life} LIFE\n")
+
             continue
-        elif g < rn :
+        elif g < rn:
             attempts += 1
             life -= 1
             print(f"{g} IS TOO SMALL")
             print(f"{attempts} ATTEMPTS")
-            print(f"LLEFT {life} LIFE")
+            print(f"LEFT {life} LIFE\n")
             continue
-            
+
 
 # >>>>>>>>> <<<<
 if __name__ == "__main__":
     main()
-
-
