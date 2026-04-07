ad72cde244274ad59230a5b5dfd7d13a3101f967
alakou <alakoustone@gmail.com>
2026-04-07 02:05:18 +0200

    evolution 2  conversion

--- convertisseur/convertisseur.py
+++ convertisseur/convertisseur.py
@@ -78,47 +78,93 @@ formulaData: Dict[str, Dict[str, Dict[str, float | int]]] = {
     },
 }
 
-categories = {i: v for i, v in enumerate((list(formulaData.keys())), start=1)}
 
-# convertMode = {i:v for i, v in enumerate(1)}
+"""
+Choisir type de conversion :  temperature
+choisir sous type : kelvin
+choisir sous sous type ; k_t_k
+entrer valeur
+affichier resulat
+continue
+
+"""
 
 
-def setCategories() -> int:
+def setCategories() -> dict:
+    categories = {i: v for i, v in enumerate(formulaData, start=1)}
     while True:
         try:
-            choice = int(input("Choisir categorie >>> "))
-            if not choice in categories:
+            choice: int = int(input("Choisir une categorie de conversion >>> "))
+            if not choice in categories.keys():
                 print("choix compris: 1 ou 2 ou 3")
-            return choice
-
+                continue
+            print(f"\nChoisir Sous Categories : ")
+            for i, v in enumerate(formulaData[categories[choice]], 1):
+                print(f"{i} > {v} ")
+            print()
+            return {1: choice, 2: categories}
+            # print(formulaData[categories[choice]][c[sub_choice]].keys())
         except ValueError:
-            print("Entrer valid choix")
+            print("Enter valid choice for categories")
 
 
-def setlabelConv(choice: int):
-    pass
-
-
-def setConversion(choice: int) -> int:
+def setSubCategories(data: dict):
+    categories: dict = data[2]
+    choice: int = data[1]
     while True:
         try:
-            setlabelConv(choice)
+            SS_CATEGORY = {
+                i: v for i, v in enumerate(formulaData[categories[choice]], 1)
+            }
 
-            conv = int(input("choisir une conversion >>> "))
+            ss_choice = int(input("Entrer votre sous categorie de conversion >>> "))
 
-        except ValueError:
-            pass
+            if ss_choice not in SS_CATEGORY.keys():
+                print("sous choix compris: 1 ou 2 ou 3")
+                continue
+            print("\nchoisir la conversion : ")
+            for i, v in enumerate(
+                formulaData[categories[choice]][SS_CATEGORY[ss_choice]], 1
+            ):
+                print(f"{i} > {v} ")
+
+            # print(list(formulaData[categories[choice]][SS_CATEGORY[ss_choice]].keys()))
+            return {1: ss_choice, 2: SS_CATEGORY, 3: categories, 4: choice}
+        except:
+            print("Enter valid choice for sub categories")
 
 
-# d = {i: v for i, v in enumerate((list(formulaData[categories.values()])), start=1)}
-d = list(formulaData.keys())
-c = {}
+def setConvertionType(data: dict):
+
+    ss_choice = data[1]
+    SS_CATEGORY = data[2]
+    categories = data[3]
+    choice = data[4]
+
+    while True:
+        try:
+            conv_choice = int(input("Entrer votre choix de conversion >>> "))
+            CONVERT = {
+                i: v
+                for i, v in enumerate(
+                    formulaData[categories[choice]][SS_CATEGORY[ss_choice]], 1
+                )
+            }
+            if conv_choice not in CONVERT.keys():
+                print("choice convert type between 1 or 2")
+                continue
+            print(formulaData[categories[choice]][SS_CATEGORY[ss_choice]][CONVERT[conv_choice]])
+            break
+        except:
+            print("Enter valid choice for convertion types")
 
 
-def con(l):
-    for i in d:
-        for v in formulaData[i].keys():
-            print(v)
+def main():
+    l = setCategories()
+    c = setSubCategories(l)
+    setConvertionType(c)
+    # pass
 
 
-con()
+if __name__ == "__main__":
+    main()
