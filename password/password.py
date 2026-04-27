ac1e15673555acb2f29615854aaaf007f7cb7048
alakou <alakoustone@gmail.com>
2026-04-27 18:37:44 +0200

    password correction v2

--- password/password.py
+++ password/password.py
@@ -21,109 +21,205 @@ Génère des mots de passe aléatoires selon les critères de l'utilisateur :
 
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation, Formatter
 import secrets, random
-from typing import Callable, Any, List
-import pyperclip
+from typing import List, Tuple
+import pyperclip as pc
 
 
-fmt: Callable[[str, Any], str] = lambda text, *args: Formatter().format(text, args)
-
-
-def number_input(text) -> int:
+def binary_input(text: str) -> bool:
+    """_summary_
+    function for asking input
+    value : int(input)  -> 0 or 1
+    Returns:
+        bool(0) = False or bool(1) = True
+    """
     while True:
         try:
-            n = int(input(text))
-            if n >= 0:
-                return n
-            print(fmt("Must be greather 0"))
+            value = int(input(text))
+            if not value in (0, 1):
+                print("Value must be 0 or 1")
+            return bool(value)
         except ValueError:
-            print(fmt("Not a number - retry"))
+            print("Not a number - retry")
 
 
-def choosePwd(n: int, l: int, arr: List[tuple]) -> str:
+def number_input(text: str, minimum_value: int = 1) -> int:
+    """_summary_
+    function for asking input
+    value : int(input)
+    value > minimum_value
+    Returns:
+        int(value)
+    """
     while True:
-        if not 0 < n <= l:
-            print(f"{n} must be between 1 and {l}")
-
-        return arr[n - 1]
-
-
-def evaluate_force(pwd: str) -> str:
-    has_lower = any(p.islower() for p in pwd)
-    has_upper = any(p.isupper() for p in pwd)
-    has_digits = sum(p.isdigit() for p in pwd)
-    has_punct = sum(p in punctuation for p in pwd)
-
-    length: int = len(pwd)
-    if length >= 20 and has_lower and has_upper and has_digits >= 3 and has_punct >= 3:
-        msg = "Tres fort"
-    elif (
-        length >= 15 and has_lower and has_upper and has_digits >= 2 and has_punct >= 2
-    ):
-        msg = "Fort"
-    elif length >= 10 and (
-        (has_lower and has_upper) or (has_digits >= 1 and has_punct >= 1)
-    ):
-        msg = "Moyen"
-    else:
-        msg = "Faible"
-
-    return msg
+        try:
+            value = int(input(text))
+            if not value >= minimum_value:
+                print("Input Value must be greather than 1")
+            return value
+        except ValueError:
+            print("Not a number - retry")
 
 
-def welcome() -> None:
-    print("#" * 40)
-    print("WELCOME TO GENESIA")
-    print("#" * 40, end="\n")
+def build_password_chars() -> Tuple[str, List[str]]:
+    """_summary_
+    function builts char contains in password
+    based on binary input response
+    Char must be :
+        ascii_lowercase (and/or) ascii_uppercase (and/or) digits (and/or) punctuation
+
+        if User choose an option (True)
+            - add this option to chars
+            - ensure that chars contains at least one char of this option
+    Returns:
+        tuple(chars: str, required_chars_per_choice:List[str])
+    """
+
+    chars: str = ""
+    required_chars_per_choice = []
+
+    choose_minus = binary_input("Choose lowercase char ? (0/1) >>> ")
+    if choose_minus:
+        chars += ascii_lowercase
+        required_chars_per_choice.append(secrets.choice(ascii_lowercase))
+
+    choose_majus = binary_input("Choose uppercase char ? (0/1) >>> ")
+    if choose_majus:
+        chars += ascii_uppercase
+        required_chars_per_choice.append(secrets.choice(ascii_uppercase))
+
+    choose_digits = binary_input("Choose digits char ? (0/1) >>> ")
+    if choose_digits:
+        chars += digits
+        required_chars_per_choice.append(secrets.choice(digits))
+
+    choose_punctuation = binary_input("Choose punctuation char ? (0/1) >>> ")
+    if choose_punctuation:
+        chars += punctuation
+        required_chars_per_choice.append(secrets.choice(punctuation))
+
+    return chars, required_chars_per_choice
 
 
+def genarate_unique_password(length: int, chars: str, required_chars: List[str]) -> str:
+    """_summary_
+    Function generate unique secure password based on
+    OS Entropie and Secrets module
+
+    arg : length: int, chars: str, required_chars: List[str]
+
+    if chars is empty  -> Error
+    if lenght > len(required_chars_per_choice) -> Error
+
+
+    Returns:
+        str(password)
+
+    """
+
+    if len(chars) == 0:
+        raise ValueError("Your Charset is Emply - Retry to chars")
+
+    if len(required_chars) > length:
+        raise ValueError("Insufficient length")
+
+    # Copy array to keep safe the orginal array
+    passwords_list = required_chars.copy()  # 4
+
+    remaining_length = length - len(passwords_list)  # l = 10 -- 10 - 3 = 7
+
+    passwords_list.extend(secrets.choice(chars) for _ in range(remaining_length))
+
+    secrets.SystemRandom().shuffle(passwords_list)
+
+    return "".join(passwords_list)
+
+
+def evaluate_password_force(password: str):
+    length = len(password)
+
+    has_lower = any(char.islower() for char in password)
+    has_upper = any(char.isupper() for char in password)
+    count_digits = sum(char.isdigit() for char in password)
+    count_punctuation = sum(char in punctuation for char in password)
+
+    score: int = 0
+
+    if length >= 10:
+        score += 1
+
+    if length >= 15:
+        score += 1
+
+    if has_lower:
+        score += 1
+
+    if has_upper:
+        score += 1
+
+    if count_digits >= 2:
+        score += 1
+
+    if count_punctuation >= 3:
+        score += 1
+
+    if score <= 2:
+        return "Faible"
+    if score <= 4:
+        return "Moyen"
+
+    if score <= 5:
+        return "Fort"
+
+    if score <= 6:
+        return "Tres fort"
+
+
+def generate_multiple_passwords(count: int, length: int, chars: str, required_chars: List[str]) -> List[str]:
+    return [
+        genarate_unique_password(length, chars, required_chars) for _ in range(count)
+    ]
+def display_passwords(passwords: List[str]) -> None:
+    
+    for index, pwd in enumerate(passwords, start=1) :
+        print(f"\t{index} - {pwd} >>> {evaluate_password_force(pwd)}")
+
+
+def choose_password(passwords:List[str])  -> str:
+    while True :
+        choice = number_input(f"Choose betwen 1 and {len(passwords)}  >>> ")
+        if 1 <= choice <= len(passwords) :
+            return passwords[choice - 1]
+        print("Invalid Choice")
+    
+        
+def copy_to_clipboard(password: str) -> None :
+    choice = binary_input("Copy to clipboard ? (0/1) >>> ")
+    
+    if choice :
+        print("Password copied")
+        pc.copy(password)
+    else :
+       print("Password did not copy") 
+    
+
+# <>
 def main() -> None:
-    welcome()
-    while True:
+    length = number_input("Set length >>> ")
 
-        q1 = number_input("\nDefinir la longueur du mp (0 = No / 1 = Yes) >> ")
-        if q1 == 1:
-            length = number_input("Votre longueur >> ")
-        else:
-            length = random.randint(5, 30)
+    chars, required_chars = build_password_chars()
 
-        char = ""
+    count = number_input("How many password generate ? >>> ")
+    x = generate_multiple_passwords(
+        count=count, length=length, chars=chars, required_chars=required_chars
+    )
+    
+    display_passwords(x)
+    
+    g = choose_password(x)
+    
+    copy_to_clipboard(g)
 
-        q2 = number_input("\nContenir des minusclues (0 = No / 1 = Yes) >> ")
-        if q2 == 1:
-            char += ascii_lowercase
-
-        q3 = number_input("\nContenir des majuscules (0 = No / 1 = Yes) >> ")
-        if q3 == 1:
-            char += ascii_uppercase
-
-        q4 = number_input("\nContenir des nombres (0 = No / 1 = Yes) >> ")
-        if q4 == 1:
-            char += digits
-
-        q5 = number_input("\nContenir des ponctuation (0 = No / 1 = Yes) >> ")
-        if q5 == 1:
-            char += punctuation
-
-        count = number_input("\nGenerer combien de mp ? >> ")
-
-        # generate: List[str] = [(i,"".join(secrets.choice(char) for _ in range(length))) for i in range(1, count + 1)]
-        generate: List[str] = [
-            "".join(secrets.choice(char) for _ in range(length))
-            for _ in range(1, count + 1)
-        ]
-
-        print("\n Vos mots de passe")
-        for i, p in enumerate(generate, 1):
-            res = evaluate_force(p)
-            print(f"{i} - {p} >>> {res} ")
-
-        choix = number_input(f"\nchoisir un mp entre de 1 et {count} ? >> ")
-
-        print(f"\nYour choice: {generate[choix -1]}")
-
-        # <>
-
-        break
+    # print()
 
 
 if __name__ == "__main__":
